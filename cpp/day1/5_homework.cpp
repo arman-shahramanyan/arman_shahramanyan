e065c0541ff85d8e784a640e4c28bbba1c6ac594
arman-shahramanyan <shahramanyan1995@mail.ru>
2022-03-03 21:58:35 +0400

    Added fifth homework

---
+++ cpp/day1/5_homework.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main  (){
+int a, b;
+int p, s;
+	cout << "Please enter rectangle parameters \n";
+	cin >> a;
+	cin >> b;
+    p=2*(a+b);
+    s=a*b;
+	cout << "Perimeter=" << p << endl;
+	cout << "Area=" << s << endl;
+
+
+return 0;
+} 
