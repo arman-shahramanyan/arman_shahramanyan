364824a5cdf3b736f320d3e5ed36d102c4d3df93
arman-shahramanyan <shahramanyan1995@mail.ru>
2022-03-10 14:12:13 +0400

    Fixed 2_perfect_numbers.cpp file

--- cpp/day4/2_perfect_numbers.cpp
+++ cpp/day4/2_perfect_numbers.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void perfect (int n) {
-  int i;
-  int arr[] = { 6, 28, 496, 8128, 33550336 };
-  for (i = 0; i < 7; i++)
-    {
-      if (n == arr[i])
-	{
-	  cout << n << "-perfect number\n";
-	  break;
-	}
-    }
-  if (n != arr[i])
-    {
-
-      cout << n << "-not perfect\n";
-    }
-
+int perfect (int n) {
+   int arr[n];
+   int x = n;
+   int sum = 0;
+  for (int i = 0; i < n; i++) {
+   if (n % x == 0 && x > 0 && x < n) {
+   arr[i] = x;
+   sum += arr[i];
+   }
+	x--;
+  }
+   return sum;
 }
 
 int main () {
   int num;
-  cout << "Enter the number \n";
-  cin >> num;
-  perfect (num);
+ 	 cout << "Enter the number \n";
+ 	 cin >> num;
+  if (perfect(num) == num ) {
+  	 cout << num << " is perfect number\n";
+  } else {
+ 	 cout << num << " is not perfect number\n";
+  }
   return 0;
 }
