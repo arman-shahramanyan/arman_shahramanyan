a523d2371a29f6e37a2f8dfdccb88beac780f4f1
Arman <shahramanyan1995@mail.ru>
2022-03-15 09:30:45 +0400

    4 and 5 exercises made a functiion

--- cpp/day7/4_revers_array.cpp
+++ cpp/day7/4_revers_array.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void revers (int array[]) {
+	int x;
+ for (int i = 0; i < 5 / 2; i++) {
+        x = *(array + 4 - i);
+        *(array + 4 - i) = *(array + i);
+        *(array + i) = x;
+ }
+ for (int i = 0; i < 5; i++) {
+       cout << *(array + i) << " ";
+ }
+}
+
 int main () {
-   int x;
    int arr[6] = {12, 5, 0, 44, 17, 29};
    for (int i = 0; i < 5; i++) {
        cout << *(arr + i) << " ";
    }
     cout << endl;
-   for (int i = 0; i < 5 / 2; i++) {
-        x = *(arr + 4 - i);
-        *(arr + 4 - i) = *(arr + i);
-        *(arr + i) = x;
-   }
+    revers(arr);
    for (int i = 0; i < 5; i++) {
        cout << *(arr + i) << " ";
    }
--- cpp/day7/5_min_max_pointers.cpp
+++ cpp/day7/5_min_max_pointers.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void max_min(int arr[]) {
+	int max = 0;
+	int min = *(arr);
+	for (int i = 0; i < 6; i++) {
+       	     cout << *(arr + i) << " ";
+      	     if (max <= *(arr + i)) {
+             max = *(arr + i);
+      	     }
+      	     if (min >= *(arr + i)) {
+             min = *(arr + i);
+      	     }
+	}
+	 cout << endl << "max=" << max << endl << "min=" << min << endl;
+ }
+
 int main () {
-   int arr[6] = {18, 51, 86, 44, 17, 29};
-   int max = 0;
-   int min = *(arr + 5);
-   for (int i = 0; i < 5; i++) {
-       cout << *(arr + i) << " ";
-       if (max <= *(arr + i)) {
-           max = *(arr + i);
-       }
-       if (min >= *(arr + i)) {
-           min = *(arr + i);
-       }
-   }
-   cout << endl << "max=" << max << endl << "min=" << min << endl;
+   int array[6] = {18, 51, 86, 44, 17, 29};
+   max_min(array);
      return 0;
 }
