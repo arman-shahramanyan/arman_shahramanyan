6fe0ce6ac5c39bc20347396064094db97b1dada2
arman-shahramanyan <shahramanyan1995@mail.ru>
2022-03-15 21:14:41 +0400

    fixed function

--- cpp/day7/4_revers_array.cpp
+++ cpp/day7/4_revers_array.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void revers (int array[]) {
-	int x;
- for (int i = 0; i < 5 / 2; i++) {
-        x = *(array + 4 - i);
-        *(array + 4 - i) = *(array + i);
-        *(array + i) = x;
- }
- for (int i = 0; i < 5; i++) {
-       cout << *(array + i) << " ";
+void revers (int *array, int size) {
+ int x;
+ for (int *ptr = array + size; ptr >= array; ptr--) {
+       cout << *ptr << " ";
  }
 }
 
 int main () {
-   int arr[6] = {12, 5, 0, 44, 17, 29};
-   for (int i = 0; i < 5; i++) {
-       cout << *(arr + i) << " ";
+   int arr[] = {12, 5, 44, 17, 29, 20};
+   int *ptr;
+   int n = sizeof(arr) / 4;
+   for (ptr = arr; ptr < arr + n; ptr++) {
+       cout << *ptr << " ";
    }
     cout << endl;
-    revers(arr);
-   for (int i = 0; i < 5; i++) {
-       cout << *(arr + i) << " ";
-   }
+    revers(arr, n - 1);
     cout << endl;
     return 0;
 }
