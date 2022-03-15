eff2d7669093a8462b95f499fbba687e93ca1dc6
arman-shahramanyan <shahramanyan1995@mail.ru>
2022-03-15 21:18:47 +0400

    fixed min_max function

--- cpp/day7/5_min_max_pointers.cpp
+++ cpp/day7/5_min_max_pointers.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void max_min(int arr[]) {
+void max_min(int *arr, int size) {
 	int max = 0;
 	int min = *(arr);
-	for (int i = 0; i < 6; i++) {
-       	     cout << *(arr + i) << " ";
-      	     if (max <= *(arr + i)) {
-             max = *(arr + i);
+	for (int *ptr = arr; ptr < arr+size; ptr++) {
+       	     cout << *ptr << " ";
+      	     if (max <= *ptr ) {
+             max = *ptr;
       	     }
-      	     if (min >= *(arr + i)) {
-             min = *(arr + i);
+      	     if (min >= *ptr) {
+             min = *ptr;
       	     }
 	}
 	 cout << endl << "max=" << max << endl << "min=" << min << endl;
  }
 
 int main () {
-   int array[6] = {18, 51, 86, 44, 17, 29};
-   max_min(array);
+   int array[] = {18, 51, 86, 44, 29};
+   int n = sizeof(array) / 4;
+   int *ptr = array;
+   max_min(ptr, n);
      return 0;
 }
