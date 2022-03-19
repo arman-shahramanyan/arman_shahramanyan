125446dec9b91f774897446f839227973d8950d4
arman-shahramanyan <shahramanyan1995@mail.ru>
2022-03-19 22:40:51 +0400

    Ended task about deposit

--- cpp/day10/2_deposit.cpp
+++ cpp/day10/2_deposit.cpp
@@ -1,68 +1,57 @@
 #include <iostream>
+#include <string>
 #include <ctime>
-#include <cstdlib>
+
 using namespace std;
 
-int income (int arr[3], double p);
+int income(int arr[3], double p);
 
 int main(int argc, char* argv[]) {
+
   int arr[3];
   for (int i = 0; i < 3; i++) {
     arr[i] = atoi(argv[i + 1]);
-    cout << arr[i] << endl;
   }
   int t = arr[0];
   double perc = (arr[1] / 12.0) / 100;
   int x = income(arr, perc) - t;
-  cout << "Profit of all months = " << x << endl;
+  cout << "Profit of " << arr[2] << " months = " << x << endl;
   return 0;
 }
+
 int income(int arr[3], double p) {
-
-   double x;
-   time_t now = time(0);
-   char* dt =ctime( &now);
-       cout << "Date of the deposit  "<< dt << endl;
-
-   int january = 1640980800;
-   int n = now;
-   int mon = 0;
-    while (january <= n) {
-       mon++;
-       n = n - 2678400;
-    }
+  double x;
+  time_t now = time(0);
+  char *dt = ctime(&now);
+  string date[5];
+  int e = 0;
+  for (int i = 0; dt[i] != '\0'; i++) {
+      if (dt[i] == ' ') {
+         e++;
+	 continue;
+      }
+      date[e] += dt[i];
+  }
+  string month[12] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+  int m , y = 0;
+  y = stoi(date[4]);
+  for (int i = 0; i < 12; i++) {
+      if (month[i] == date[1]) {
+         m = i; 
+      }
+  }
+  cout << "Start deposit  " << "==>" << month[m] << " " << date[2] << " " << date[4] << endl;
   for (int i = 0; i < arr[2]; i++) {
-     x = arr[0] * p;
-     switch (mon) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-          now += 2678400;
-          break;
-        case 2:
-          now += 2419200;
-          break;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-          now += 2592000;
-          break;
-        }
-     char * t = ctime(&now);
-     cout << "Sum of the month = " << arr[0];
-     cout << " Income of the month =  " << x  << "  "<< t ;
-     mon++;
-     if (mon > 12 ){
-       mon = 1;
-     }
+    m++;
+    if (m == 12) {
+       y++;
+       m = 0;
+    }
+    x = arr[0] * p;
+    	cout << "Sum of the " << month[m] << " ==> " << arr[0] << " Income of the " << month[m] << " ==> " << x;
+    	cout << "    " << month[m] << " " << date[2] << " " << y << endl;
     arr[0] += x;
-       cout << endl;
+    	cout << endl;
   }
   return arr[0];
 }
-
