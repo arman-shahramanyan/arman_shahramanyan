d0158c85f004355e0eea6383881911eec7f0cb5e
arman-shahramanyan <shahramanyan1995@mail.ru>
2022-03-25 00:21:17 +0400

    translater program

---
+++ cpp/day13/translation.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <locale>
+
+using namespace std;
+
+void translate1(string str, string s[12]) {
+int count = 0;
+string elem;
+ for (int i = 0; i <= str.length(); i++) {
+    if (str[i] != ' ' || str[i] != '\0') {
+      elem[i] = str[i];
+    }
+    if (str[i] == ' ' || str[i] == '\0') {
+    	break;
+    }
+  }
+  for(int i = 0; i < 12; i++) {
+      for(int j = 0; s[i][j] != '-'; j++) {
+          if (s[i][j] == str[j]) {
+              count++;
+          }
+          if (count == str.length()) {
+              count += 2;
+             for (int m = count; s[i][m] != '\0'; m ++) {
+                  cout << s[i][m];
+             }
+                  cout << endl;
+          }
+      }
+  }
+}
+
+int main() {
+setlocale(LC_ALL,"Armenian");
+
+  cout << "					DICTIONARY \n" << endl;
+  cout << "				Select a dictionary mode\n";
+  cout << "				hy-en mode(input 1)\n";
+  cout << "				en-hy mode(input 2)\n";
+  
+  int mode;
+      cin >> mode;
+  string text;
+      cout << "Enter a word to translate: "; 
+  getline(cin, text);
+  getline(cin, text);
+  
+
+      cout << endl; 
+  string s1[12];
+  string s2[12];
+
+  ifstream f1;
+  f1.open("hy-en.txt");
+
+  for (int i = 0; i < 12; i++) {
+    f1 >> s1[i];
+  }
+  f1.close();
+
+  ifstream f2;
+  f2.open("en-hy.txt");
+
+  for (int i = 0; i < 12; i++) {
+    f2 >> s2[i]; 
+  }
+  f2.close();
+  if (mode == 1) {
+    for (int i = 0; i <= text.length(); i++) {
+      if (text[i] == ' ' || text[i] == '\0') {
+          translate1(text, s1);    
+      }
+    }  
+  }
+
+  if (mode == 2) {
+    for (int i = 0; i <= text.length(); i++) {
+      if (text[i] == ' ' || text[i] == '\0') {
+          translate1(text, s2);    
+      }
+    }
+  }
+ 
+ return 0;
+}
