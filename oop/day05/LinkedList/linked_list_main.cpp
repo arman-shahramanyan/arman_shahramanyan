c18f89ed0805008f5ff1a16bc1e859827060748e
arman-shahramanyan <shahramanyan1995@mail.ru>
2022-04-27 03:14:32 +0400

    add exception

--- oop/day05/LinkedList/linked_list_main.cpp
+++ oop/day05/LinkedList/linked_list_main.cpp
@@ -5,20 +5,28 @@ using namespace std;
 
 int main()
 {
-    LinkedList a;
-    a.AddFront(5);
-    a.AddBack(88);
-    a.AddNode(9,1);
-    a.AddNode(15, 0);
-    a.Print();
-    a.RemoveFront();
-    a.Print();
-    a.RemoveBack();
-    a.Print();
-    a.AddNode(3, 1);
-    a.Print();
-    cout << "Number of nodes in the list = " << a.Count() << endl;
-    a.RemoveNode(2);
-    a.Print();
+    try 
+    {
+        LinkedList a;
+        a.AddFront(5);
+        a.AddBack(88);
+        a.AddNode(9,1);
+        a.AddNode(15, 0);
+        a.Print();
+        a.RemoveFront();
+        a.Print();
+        a.RemoveBack();
+        a.Print();
+        a.AddNode(3, 1);
+        a.Print();
+        cout << "Number of nodes in the list = " << a.Count() << endl;
+        a.RemoveNode(2);
+        a.RemoveNode(5); //for exception
+        a.Print();
+    }
+    catch (exception err)
+    {
+        cout << "The index entered does not match the number of nodes in the list \n";
+    }
 return 0;
 }
